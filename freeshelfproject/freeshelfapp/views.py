26b214f58d504038025eb81e62c27fdd0d0a432b
Juan Polanco <juanpolanco5008@gmail.com>
2019-03-11 16:35:29 -0400

    added "hello welcome to this blank page"

--- freeshelfproject/freeshelfapp/views.py
+++ freeshelfproject/freeshelfapp/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
-    return HttpResponse("hell welcome to this blank page!")
+    return HttpResponse("hello welcome to this blank page!")
