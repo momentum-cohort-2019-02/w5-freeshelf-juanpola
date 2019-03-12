4d989c980459014fdaaca00c60124e770268bd5b
Juan Polanco <juanpolanco5008@gmail.com>
2019-03-11 21:04:58 -0400

    add book class

--- freeshelfproject/freeshelfapp/models.py
+++ freeshelfproject/freeshelfapp/models.py
@@ -1,4 +1,26 @@
 from django.db import models
+from datetime import date
+from django.urls import reverse
+
 
 # Create your models here.
+
+class Book(models.Model):
+    """ This class represents a book"""
+    title = models.CharField(max_length=200, null=True, blank=True)
+    author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
+    book_urls = models.URLField(max_length=150, null=True)
+    book_description = models.CharField(max_length=5000, null=True, blank=True)
+    date_added = models.DateField('Date Added', auto_now_add=True)
+
+    def __str__(self):
+        """ String """
+        return self.title
+    
+
+
+
+
+
+
 
