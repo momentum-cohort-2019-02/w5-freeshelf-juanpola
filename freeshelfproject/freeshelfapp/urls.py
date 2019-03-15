1cb183cf07733061e86a4c1d1e3fc3918281756d
Juan Polanco <juanpolanco5008@gmail.com>
2019-03-14 21:34:48 -0400

    Working on Regsiter/ login

--- freeshelfproject/freeshelfapp/urls.py
+++ freeshelfproject/freeshelfapp/urls.py
@@ -3,10 +3,11 @@ from django.urls import path
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
-
+from django.contrib.auth.views import login 
 
 urlpatterns = [
      # path('', views.index, name='index'),
      path('',views.index, name='index')
+     # url(r'^login/$',login,{'template_name':accounts/login.html})
 ] 
 # + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
